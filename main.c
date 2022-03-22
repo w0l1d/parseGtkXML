520a26bd51243bb9f912b204c81875cb59532a7f
W0L1D <ahdoufwalid@gmail.com>
2022-03-22 14:53:28 +0100

    problem : apply container child properties

--- main.c
+++ main.c
@@ -23,33 +23,37 @@ activate(GtkApplication *app,
     GtkWidget *label;
     GtkWidget *menuBtn;
 
+
+
+
     GtkWidget *xmlWindow = macro_getWindow("simplest interface.glade");
-    window = gtk_application_window_new(app);
+//    window = gtk_application_window_new(app);
+//
+//
+//    label = gtk_label_new("My Menu");
+//
+//    menuBtn = gtk_menu_button_new();
+//    gtk_menu_button_set_direction(GTK_MENU_BUTTON(menuBtn), GTK_ARROW_RIGHT);
+//    gtk_menu_button_set_popup(GTK_MENU_BUTTON(menuBtn), getMenuExample());
+//
+//    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+//    gtk_box_pack_start(GTK_BOX(box), label, TRUE, TRUE, 0);
+//    gtk_box_pack_start(GTK_BOX(box), menuBtn, TRUE, TRUE, 0);
+//
+//
+//    button = gtk_flow_box_new();
+//
+//    gtk_container_add(GTK_CONTAINER (button), box);
+//
+//
+//
+//
+//
+//
+//
+//    gtk_container_add(GTK_CONTAINER (window), button);
+//    gtk_widget_show_all(window);
 
-
-    label = gtk_label_new("My Menu");
-
-    menuBtn = gtk_menu_button_new();
-    gtk_menu_button_set_direction(GTK_MENU_BUTTON(menuBtn), GTK_ARROW_RIGHT);
-    gtk_menu_button_set_popup(GTK_MENU_BUTTON(menuBtn), getMenuExample());
-
-    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_box_pack_start(GTK_BOX(box), label, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(box), menuBtn, TRUE, TRUE, 0);
-
-
-    button = gtk_flow_box_new();
-
-    gtk_container_add(GTK_CONTAINER (button), box);
-
-
-
-
-
-
-
-    gtk_container_add(GTK_CONTAINER (window), button);
-    gtk_widget_show_all(window);
     gtk_widget_show_all(xmlWindow);
 }
 
--- parsers.h
+++ parsers.h
@@ -25,7 +25,7 @@
 
 
 
-
+GtkWidget *macro_TransWidget(xmlNode *node);
 
 
 /*
@@ -83,24 +83,174 @@ void macro_cleanupXML(xmlDoc *doc) {
     xmlCleanupParser();
 }
 
+GObject *macro_TransObjAttrs(xmlNode *node) {
+    GObject *widget;
 
 
-GtkWidget *macro_getWindow(gchar *filename) {
-    xmlNode *root;
-    root = macro_getRootElem(filename);
+    //NULL NODE
+    if (!node) {
+        fprintf(stderr, "NULL NODE\n");
+        exit(-1);
+    }
 
 
+    xmlChar *typeName = xmlGetProp(node, (const xmlChar *) ATTR_CLASS);
 
-    /**
-     * BUILD Window from xml
-     */
-    GtkWidget *window;
-    window = macro_TransWidget(root->children);
+    printf("\n object name : %s\n", typeName);
+    GType gType = macro_getTypeFromName((const gchar *) typeName);
 
 
-    macro_cleanupXML(root->doc);
-    return window;
+    g_assert(gType != 0);
+
+    widget = g_object_new(gType, NULL);
+
+    g_assert(widget != NULL);
+    /// TODO :: PARSE THE REST OF ATTRIBUTES IF EXISTS
+
+    return widget;
 }
+
+void macro_ApplyObjProp(GObject *object, xmlNode *node) {
+    GValue value = G_VALUE_INIT;
+
+    //get property name from xml
+    const gchar *property = (gchar *) xmlGetProp(node, (const xmlChar *) ATTR_NAME);
+    //get property value
+    const gchar *content = (gchar *) xmlNodeGetContent(node);
+    //get property default value from object
+    g_object_get_property(object, property, &value);
+    //translate xml value from string to correct value
+    macro_valueFromStringType(value.g_type, content, &value);
+    //set value to the object
+    g_object_set_property(object, property, &value);
+}
+
+void macro_ApplyObjChildProp(GObject *object, GObject *child, xmlNode *node) {
+
+
+    //get property name from xml
+    const gchar *property = (gchar *) xmlGetProp(node, (const xmlChar *) ATTR_NAME);
+    //get property value
+    const gchar *content = (gchar *) xmlNodeGetContent(node);
+
+    GValue value = G_VALUE_INIT;
+    GParamSpec *pspec;
+    pspec = gtk_container_class_find_child_property (G_OBJECT_GET_CLASS (object),
+                                                     property);
+
+
+//    gtk_container_class_install_child_property(GTK_CONTAINER_GET_CLASS(object), pspec->param_id, pspec);
+//    g_value_init (&value, pspec->value_type);
+
+    g_value_set_gtype(&value, pspec->value_type);
+
+    //get property default value from object
+//    gtk_container_child_get_property(GTK_CONTAINER(object), GTK_WIDGET(child),property,&value);
+
+    g_print("\nchild property %s, content %s, type %s, default %s, new\n", property, content, g_type_name(pspec->value_type), g_value_get_string(&value));
+    //translate xml value from string to correct value
+    macro_valueFromStringType(pspec->value_type, content, &value);
+
+
+    g_print(" %s ", g_value_get_string(&value));
+
+    //set value to the object
+    g_object_set_property(child,property,&value);
+}
+
+void macro_applyChildProps(GObject *object, GObject *child, xmlNode *node) {
+    xmlNode *curNode;
+    for (curNode = node->children; curNode; curNode = curNode->next) {
+        if (!xmlStrcasecmp(curNode->name, (xmlChar *) TAG_PROPERTY)) {
+            macro_ApplyObjChildProp(object, child, curNode);
+        }
+    }
+}
+
+void macro_addChild(GObject *object, xmlNode *node) {
+    xmlNode *curNode;
+    GObject *child;
+    for (curNode = node->children; curNode; curNode = curNode->next) {
+        //looking for child
+        if (!xmlStrcasecmp(curNode->name, (xmlChar *) TAG_OBJECT)) {
+            child = (GObject *) macro_TransWidget(curNode);
+            gtk_container_add(GTK_CONTAINER(object), GTK_WIDGET(child));
+
+        }
+        //looking for child properties
+        if (!xmlStrcasecmp(curNode->name, (xmlChar *) TAG_PACKING) && child) {
+            macro_applyChildProps(object, child, curNode);
+            child = NULL;
+        }
+    }
+}
+
+    void macro_parseChildrenTags(GObject *object, xmlNode *node) {
+        g_assert(object != NULL);
+
+
+        xmlNode *curNode = node->children;
+        while (curNode) {
+            //TODO :: COMPLETE CODE HERE
+            // handle all tags
+
+            if (!xmlStrcasecmp(curNode->name, (xmlChar *) TAG_PROPERTY))
+                macro_ApplyObjProp(object, curNode);
+            else if (!xmlStrcasecmp(curNode->name, (xmlChar *) TAG_CHILD))
+                macro_addChild(object, curNode);
+
+            curNode = curNode->next;
+        }
+
+    }
+
+
+    GtkWidget *macro_TransWidget(xmlNode *node) {
+        xmlNode *curNode;
+        curNode = node;
+        GObject *widget = NULL;
+
+        //NULL NODE
+        if (!curNode) {
+            fprintf(stderr, "NULL NODE\n");
+            exit(-1);
+        }
+
+        //ignoring none object tags
+        while (curNode && xmlStrcasecmp(curNode->name, (const xmlChar *) TAG_OBJECT)) {
+            curNode = curNode->next;
+        }
+        //NO OBJECT NODE FOUNT
+        if (!curNode) {
+            fprintf(stderr, "NO OBJECT TAG FOUND\n");
+            exit(-1);
+        }
+
+        widget = macro_TransObjAttrs(curNode);
+
+        macro_parseChildrenTags(widget, curNode);
+
+
+        return GTK_WIDGET(widget);
+    }
+
+
+    GtkWidget *macro_getWindow(gchar *filename) {
+        xmlNode *root;
+        root = macro_getRootElem(filename);
+
+
+
+        /**
+         * BUILD Window from xml
+         */
+        GtkWidget *window;
+        window = macro_TransWidget(root->children);
+
+
+        macro_cleanupXML(root->doc);
+        return window;
+    }
 
 
 
